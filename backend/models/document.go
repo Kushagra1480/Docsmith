09859c26cf7d4347f0e9e0fde2b8745d931e1082
Kushagra1480 <kushagrakartik1480@gmail.com>
2025-03-30 23:38:06 -0700

    Defined websockets

--- backend/models/document.go
+++ backend/models/document.go
@@ -3,7 +3,7 @@ package models
 type Document struct {
 	ID string `json:"id"`
 	UserID string `json:"user_id"`
-	Title string `json:title"`
+	Title string `json:"title"`
 	Content string `json:"content"`
 	UpdatedAt string `json:"updated_at"`
 }
---
+++ backend/ws/hub.go
@@ -0,0 +1,143 @@
+package ws
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type Message struct {
+	Type string                 `json:"type"`
+	Data map[string]interface{} `json:"data"`
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan *Message
+	register   chan *Client
+	unregister chan *Client
+	mutex      sync.Mutex
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients: make(map[*Client]bool),
+		broadcast: make(chan *Message),
+		register: make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mutex.Lock()
+			h.clients[client] = true
+			h.mutex.Unlock()
+		case client := <-h.unregister:
+			h.mutex.Lock()
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)	
+			}
+			h.mutex.Unlock()
+		case message := <-h.broadcast:
+			msgJSON, err := json.Marshal(message)
+			if err != nil {
+				log.Printf("Error marshalling message: %v", err)
+				continue
+			}
+
+			h.mutex.Lock()
+			for client := range h.clients {
+				select {
+				case client.send <- msgJSON:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+			h.mutex.Unlock()
+		}
+	}
+	
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("Error: %v", err)
+			}
+			break
+		}
+
+		var msg Message
+		if err := json.Unmarshal(message, &msg); err != nil {
+			log.Printf("Error unmarshalling message: %v", err)
+			continue
+		}
+		c.hub.broadcast <- &msg
+	}
+}
+
+func (c *Client) writePump() {
+	defer func () {
+		c.conn.Close()
+	}()
+
+	for {
+		message, ok := <-c.send
+		if !ok {
+			c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+			return 
+		}
+
+		if err := c.conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			return
+		}
+	}
+}
+
+func ServeWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := &Client{
+		hub: hub,
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
+}
